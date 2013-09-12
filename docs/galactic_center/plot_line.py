f2c5dc2b539e352abdd9d070e64239d74a9f6c47
Christoph Deil <Deil.Christoph@gmail.com>
2013-09-12 14:18:20 +0200

    Update galactic center line docs

--- docs/galactic_center/plot_line.py
+++ docs/galactic_center/plot_line.py
@@ -8,9 +8,18 @@ Christoph Deil, 2013-09-12
 import pyfits
 import matplotlib.pyplot as plt
 
-events = pyfits.getdata('line_gtmktime.fits')
-energy = events.data['ENERGY']
-print('Number of events: ', len(energy))
+MEV_TO_GEV = 1e-3
+E_MIN, E_MAX = 37.5, 222.5
 
-plt.hist(energy)
-plt.savefig('line_spectrum.png')
+events = pyfits.getdata('line_gtmktime.fits')
+energy = MEV_TO_GEV * events.field('ENERGY')
+selection = (energy > E_MIN) & (energy < E_MAX)
+n_events = len(energy[selection]) 
+print('Number of events: {0}'.format(n_events))
+
+plt.figure(figsize=(8, 5))
+plt.hist(energy, bins=37, range=(E_MIN, E_MAX))
+plt.xlabel('Energy (GeV)')
+plt.ylabel('Events / 5 GeV')
+plt.xlim(E_MIN, E_MAX)
+plt.savefig('gc_line_matplotlib.png')
